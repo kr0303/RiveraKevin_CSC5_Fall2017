89d4daa822f33323717ca12e48e04b3ec604d714
Kevin Rivera <kr0303@outlook.com>
2017-11-05 12:52:06 -0800

    Signed-off-by: Kevin Rivera <kr0303@outlook.com>

--- Project/Project 1 - 21/main.cpp
+++ Project/Project 1 - 21/main.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 using namespace std;
 
+// bool to check if number is even
 bool isEven(int n)
 {
     if (n%2==0)
@@ -16,109 +17,106 @@ bool isEven(int n)
 
 int main()
 {
-    // Set as CONSTANT for Random number generation
-    int const MIN_NUMBER = 0, MAX_NUMBER = 36;
-    int number;
-    int random;
+    // constant for the random numbers to be generated
+    int const MIN_NUM = 0, MAX_NUM = 36;
+    int num;    // number for input for number gametype
+    int rng;    // random number to be generated
 
-    float bet, winnings = 0;
+    float bet, wins = 0; // bet inputted, total earned/lost
 
-    // Use arrays for player decision
-    char gametype[2];
+    // array for player decisions (number, even, odd)
+    char gamet[3];
 
-    // Main Menu Screen
+    // display main menu
     cout << "Welcome to Roulette!\n\n";
-    cout << "How much would you like to bet?\n$";
+                                                                // balance //
+    // player inputs their money for bet
+    cout << "How much would you like to bet?\n$"; 
     cin >> bet;
-
+    
+    // player chooses gametype
     cout << "Betting on a specific number (N), odd numbers(O), or even numbers(E)? ";
-    cin >> gametype;
+    cin >> gamet;
 
-    // User selects a specific number to place bet on
-    if(!strcmp(gametype,"N")||(!strcmp(gametype,"n")))
+    // run this if player chose inputted N or n
+    if(!strcmp(gamet,"N")||(!strcmp(gamet,"n")))
     {
-        cout << "What number would you like to bet on? ";
-        cin >> number;
-         
+        cout << "What number would you like to bet on? ";   // what number to bet on
+        cin >> num;
+        
+        // random number generator between 0-36
         srand(time(NULL));
-        random = rand() % (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
+        rng = rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM;
 
-        cout << "The ball landed on " << random << "\n";
+        cout << "The ball landed on " << rng << "\n";
 
-    // Lose
-    if(number != random)
+    // loss
+    if(num != rng)
     {
         cout << "You lose $" << bet << "\n";
-        winnings -= bet;
+        wins -= bet;    // add -bet to winnings
     }
-    // Win
+    // win
     else
     {
         cout << "You win $" << 35*bet << endl;
-        winnings += 35*bet;
+        wins += 35*bet; // multiply bet times 35 and add to winnings
     }
     }
-
-// User selects even or odd
-    if((!strcmp(gametype,"E")||(!strcmp(gametype,"e"))))
+    
+    // inputs E or e for gametype
+    if((!strcmp(gamet,"E")||(!strcmp(gamet,"e"))))
     {
-
+        // random number generator between 1-36
         srand(time(NULL));
-        random = rand() % (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
-        cout << "The ball landed on " << random << endl;
+        rng = rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM;
+        cout << "The ball landed on " << rng << endl;
 
-        // selects EVEN
-        if(gametype == "E"||"e")
+        if(gamet == "E"||"e")
         {
-        // even win
-            if(isEven(random))
+            // win
+            if(isEven(rng))
             {
                 cout << "You win $" << bet << endl;
-                winnings += bet;
+                wins += bet; // add bet to winnings
             }
-        // even lose
+            // loss
             else
             {
                 cout << "You lose $" << bet << endl;
-                winnings -= bet;
+                wins -= bet; // add -bet to winnings
             }
         }
-
-        
     }
     
-    if((!strcmp(gametype,"O")||(!strcmp(gametype,"o"))))
+    // input O or o for gametype
+    if((!strcmp(gamet,"O")||(!strcmp(gamet,"o"))))
     {
-
+        // random number generator between 1-36
         srand(time(NULL));
-        random = rand() % (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
-        cout << "The ball landed on " << random << endl;
-
-        // selects odd
-        if(gametype == "O"||"o")
-        {
-        // even win
-            if(isEven(random))
-            {
+        rng = rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM;
+        cout << "The ball landed on " << rng << endl;
+            
+            // loss
+            if(isEven(rng)) // check if random number is even
+            {   // if even, then lose because betted on odd
                 cout << "You lost $" << bet << endl;
-                winnings -= bet;
+                wins -= bet;    // add bet to winnings
             }
-        // even lose
+            // win
             else
             {
                 cout << "You win $" << bet << endl;
-                winnings += bet;
+                wins += bet;    // add -bet to winnings
             }
-        }
-
-        
     }
 
-    // Final Results
-    if(winnings < bet){
-        cout << "You lost a total of $" << abs(winnings);
-    }
+
+    // final results
+    if(wins < bet){
+        cout << "You lost a total of $" << abs(wins); // absolute value used
+    }                                                 // to not show negative
     else
-        cout << "You won a total of $" << winnings;
+        cout << "You won a total of $" << wins;
     return 0;
 }
